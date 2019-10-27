49b3400adf7a9a14220d09f6782b9747261e964e
Anna Wijetunga <annawijetunga@gmail.com>
2019-10-27 00:55:12 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,33 @@
 def addition(num1, num2)
-
+  num1 = 5
+  num2 = 4
+  sum = num1 + num2
+  puts sum 
+  sum 
 end
 
 def subtraction(num1, num2)
-
+  num1 = 10 
+  num2 = 5
+  difference = num1 - num2
+  puts difference 
+  difference  
 end
 
 def division(num1, num2)
-
+  num1 = 50
+  num2 = 2 
+  product = num1 / num2
+  puts product
+  product
 end
 
 def multiplication(num1, num2)
-
+  num1 = 4
+  num2 = 30
+  product = num1 * num2
+  puts product
+  product
 end
 
 def modulo(num1, num2)
